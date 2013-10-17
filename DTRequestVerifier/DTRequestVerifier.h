4c3f6fb4bad9b66814131cc6f3e176f755e848a5
Denys Telezhkin <strangervir@gmail.com>
2013-10-17 17:14:22 +0300

    add documentation

--- DTRequestVerifier/DTRequestVerifier.h
+++ DTRequestVerifier/DTRequestVerifier.h
@@ -12,42 +12,66 @@ typedef NS_ENUM(NSInteger, DTBodySerializationType) {
     DTBodySerializationTypePlist
 };
 
+/**
+ `DTRequestVerifier` class is used to verify NSURLRequests. Set all desired properties you wan't to verify on NSURLRequest and call -verifyRequest: method.
+ */
+
 @interface DTRequestVerifier : NSObject
 
-+(instancetype)verifier;
-
--(BOOL)verifyRequest:(NSURLRequest *)request;
-
-
-/*
- HTTP Method, that will be checked on request.
- 
- @"GET" by default.
+/**
+ NSURLRequest expected HTTPMethod. Defaults to @"GET".
  */
 @property (nonatomic, strong) NSString * HTTPMethod;
 
+/**
+ NSURLRequest expected host.
+ */
 @property (nonatomic, strong) NSString * host;
-@property (nonatomic, strong) NSString * path;
-
-@property (nonatomic, strong) NSDictionary * HTTPHeaderFields;
-
 
 /**
- Query params keys should be NSStrings
+ NSURLRequest expected path.
+ */
+@property (nonatomic, strong) NSString * path;
+
+/**
+ NSURLRequest expected HTTPHeaderFields. Only header fields, that are present in this NSDictionary are checked. Other header fields, present on NSURLRequest allHTTPHeaderFields, are ignored.
+ */
+@property (nonatomic, strong) NSDictionary * HTTPHeaderFields;
+
+/**
+ NSURLRequest expected query parameters. All keys and values should be NSStrings, otherwise verification will fail. Query to verify is received from NSURL -query method.
  */
 @property (nonatomic, strong) NSDictionary * queryParams;
 
+/**
+ NSURLRequest expected body parameters. Three types of serialization supported: raw parameters, JSON and PList data.
+ */
 @property (nonatomic, strong) NSDictionary * bodyParams;
 
+/**
+ Type of serialization to use when serializing HTTPBody data. Defaults to `DTBodySerializationTypeJSON`.
+ */
 @property (nonatomic, assign) DTBodySerializationType bodySerializationType;
 
-
-/*
- Set this property to NO if you don't need log messages to be printed.
+/**
+ Set this property to NO if you don't need verification errors log messages to be printed.
  
- Default value is YES;
+ Default value is YES.
  */
 @property (nonatomic, assign) BOOL loggingEnabled;
 
+/**
+ Creates and returns `DTRequestVerifier` object.
+ */
++(instancetype)verifier;
+
+/**
+ Verifies passed request, using all properties, that were previously set on `DTRequestVerifier` instance. If verification fails, message explaining error is logged to console, and method returns NO. If all expectations were met, method returns YES.
+ 
+ @param request NSURLRequest object to verify
+ @return result of verification
+ */
+
+-(BOOL)verifyRequest:(NSURLRequest *)request;
 
 @end
